2f631f1fd05ecea7a0b54c251683c51c0fd37cae
Denis Zharikov <d2718nis@gmail.com>
2017-05-12 03:31:30 +0300

    Implement synchronous scroll

--- src/MarkdownPreviewer.js
+++ src/MarkdownPreviewer.js
@@ -51,12 +51,47 @@ Unordered list:
     });
   }
 
+  handleOnMouseEnter(target) {
+    target.addEventListener('scroll',
+      target.id === 'input-field' ? this.handleInputScroll : this.handleOutputScroll);
+    target.removeEventListener('scroll',
+      target.id === 'input-field' ? this.handleOutputScroll : this.handleInputScroll);
+  }
+
+  handleOnMouseLeave(target) {
+    target.removeEventListener('scroll',
+      target.id === 'input-field' ? this.handleInputScroll : this.handleOutputScroll);
+  }
+
+  handleInputScroll() {
+    const input = document.getElementById('input-field');
+    const output = document.getElementById('output-div');
+    output.scrollTop = Math.floor(input.scrollTop * (output.clientHeight - output.scrollHeight) /
+      (input.clientHeight - input.scrollHeight));
+  }
+
+  handleOutputScroll() {
+    const input = document.getElementById('input-field');
+    const output = document.getElementById('output-div');
+    input.scrollTop = Math.floor(output.scrollTop * (input.clientHeight - input.scrollHeight) /
+      (output.clientHeight - output.scrollHeight));
+  }
+
   render() {
     return (
       <div className="MarkdownPreviewer">
         <h1>Markdown Previewer</h1>
-        <MarkdownInput rawText={this.state.rawText} onChange={(value) => this.handleOnChange(value)} />
-        <MarkdownOutput rawText={marked(this.state.rawText)} />
+        <MarkdownInput
+          rawText={this.state.rawText}
+          onChange={(value) => this.handleOnChange(value)}
+          onMouseEnter={(target) => this.handleOnMouseEnter(target)}
+          onMouseLeave={(target) => this.handleOnMouseLeave(target)}
+        />
+        <MarkdownOutput
+          rawText={marked(this.state.rawText)}
+          onMouseEnter={(target) => this.handleOnMouseEnter(target)}
+          onMouseLeave={(target) => this.handleOnMouseLeave(target)}
+        />
       </div>
     );
   }
@@ -67,7 +102,13 @@ class MarkdownInput extends Component {
     return (
       <div className="MarkdownInput col-xs-12 col-sm-6">
         <h2>Input</h2>
-        <textarea value={this.props.rawText} onChange={(event) => this.props.onChange(event.currentTarget.value)}></textarea>
+        <textarea
+          id="input-field"
+          value={this.props.rawText}
+          onChange={(event) => this.props.onChange(event.currentTarget.value)}
+          onMouseEnter={(event) => this.props.onMouseEnter(event.currentTarget)}
+          onMouseLeave={(event) => this.props.onMouseLeave(event.currentTarget)}
+        ></textarea>
       </div>
     );
   }
@@ -78,7 +119,12 @@ class MarkdownOutput extends Component {
     return (
       <div className="MarkdownOutput col-xs-12 col-sm-6">
         <h2>Output</h2>
-        <div dangerouslySetInnerHTML={{__html: this.props.rawText}}></div>
+        <div
+          id="output-div"
+          dangerouslySetInnerHTML={{__html: this.props.rawText}}
+          onMouseEnter={(event) => this.props.onMouseEnter(event.currentTarget)}
+          onMouseLeave={(event) => this.props.onMouseLeave(event.currentTarget)}
+        ></div>
       </div>
     );
   }
