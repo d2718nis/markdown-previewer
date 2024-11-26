ff8347c4888ef1bcc12acbf0660c58fe13d675b5
Denis Zharikov <d2718nis@gmail.com>
2024-11-26 19:15:34 +0400

    Migrate to Vite

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MarkdownPreviewer from './MarkdownPreviewer';
-import './index.css';
-
-ReactDOM.render(
-  <MarkdownPreviewer />,
-  document.getElementById('root')
-);
